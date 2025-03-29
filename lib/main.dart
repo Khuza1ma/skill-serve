965f4442e7ec371a24275db52bee3c301bcb51e4
Khuza1ma <khuzaimanrko@gmail.com>
2025-03-29 15:24:23 +0530

    Changed admin panel template

--- lib/main.dart
+++ lib/main.dart
@@ -1,78 +1,37 @@
-import 'package:flareline/core/theme/global_theme.dart';
-import 'package:flareline_uikit/service/localization_provider.dart';
-import 'package:flareline/routes.dart';
-import 'package:flareline_uikit/service/theme_provider.dart';
 import 'package:flutter/material.dart';
-import 'package:flareline/flutter_gen/app_localizations.dart';
-import 'package:get/get.dart';
-import 'package:get_storage/get_storage.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
-import 'package:window_manager/window_manager.dart';
+import 'package:skill_serve/screens/main/main_screen.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
+import 'constants.dart';
+import 'controllers/menu_app_controller.dart';
 
-  await GetStorage.init();
-
-  if (GetPlatform.isDesktop && !GetPlatform.isWeb) {
-    await windowManager.ensureInitialized();
-
-    WindowOptions windowOptions = const WindowOptions(
-      size: Size(1080, 720),
-      minimumSize: Size(480, 360),
-      center: true,
-      backgroundColor: Colors.transparent,
-      skipTaskbar: false,
-      // titleBarStyle: TitleBarStyle.hidden,
-    );
-    windowManager.waitUntilReadyToShow(windowOptions, () async {
-      await windowManager.show();
-      await windowManager.focus();
-    });
-  }
-
+void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  MyApp({super.key});
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Admin Panel',
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: bgColor,
+        textTheme: GoogleFonts.poppinsTextTheme(Theme.of(context).textTheme)
+            .apply(bodyColor: Colors.white),
+        canvasColor: secondaryColor,
+      ),
+      home: MultiProvider(
         providers: [
-          ChangeNotifierProvider(create: (_) => ThemeProvider(_)),
-          //theme
-          ChangeNotifierProvider(create: (_) => LocalizationProvider(_)),
-          //localizationen ai
+          ChangeNotifierProvider(
+            create: (context) => MenuAppController(),
+          ),
         ],
-        child: Builder(builder: (context) {
-          context.read<LocalizationProvider>().supportedLocales =
-              AppLocalizations.supportedLocales;
-          return MaterialApp(
-            navigatorKey: RouteConfiguration.navigatorKey,
-            restorationScopeId: 'rootFlareLine',
-            title: 'FlareLine',
-            debugShowCheckedModeBanner: false,
-            initialRoute: '/',
-            localizationsDelegates: AppLocalizations.localizationsDelegates,
-            locale: context.watch<LocalizationProvider>().locale,
-            supportedLocales: AppLocalizations.supportedLocales,
-            onGenerateRoute: (settings) =>
-                RouteConfiguration.onGenerateRoute(settings),
-            themeMode: context.watch<ThemeProvider>().isDark
-                ? ThemeMode.dark
-                : ThemeMode.light,
-            theme: GlobalTheme.lightThemeData,
-            darkTheme: GlobalTheme.darkThemeData,
-            builder: (context, widget) {
-              return MediaQuery(
-                data: MediaQuery.of(context)
-                    .copyWith(textScaler: TextScaler.noScaling),
-                child: widget!,
-              );
-            },
-          );
-        }));
+        child: MainScreen(),
+      ),
+    );
   }
 }
