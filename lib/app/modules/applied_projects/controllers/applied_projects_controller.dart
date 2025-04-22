d59420093855188bdda406c6d0617359dc05e005
Khuza1ma <khuzaimanrko@gmail.com>
2025-04-22 22:04:40 +0530

    Refactor: Implemented empty controllers for all modules

--- lib/app/modules/applied_projects/controllers/applied_projects_controller.dart
+++ lib/app/modules/applied_projects/controllers/applied_projects_controller.dart
@@ -1,23 +1,3 @@
 import 'package:get/get.dart';
 
-class AppliedProjectsController extends GetxController {
-  //TODO: Implement AppliedProjectsController
-
-  final count = 0.obs;
-  @override
-  void onInit() {
-    super.onInit();
-  }
-
-  @override
-  void onReady() {
-    super.onReady();
-  }
-
-  @override
-  void onClose() {
-    super.onClose();
-  }
-
-  void increment() => count.value++;
-}
+class AppliedProjectsController extends GetxController {}
--- lib/app/modules/projects_details/controllers/projects_details_controller.dart
+++ lib/app/modules/projects_details/controllers/projects_details_controller.dart
@@ -1,23 +1,3 @@
 import 'package:get/get.dart';
 
-class ProjectsDetailsController extends GetxController {
-  //TODO: Implement ProjectsDetailsController
-
-  final count = 0.obs;
-  @override
-  void onInit() {
-    super.onInit();
-  }
-
-  @override
-  void onReady() {
-    super.onReady();
-  }
-
-  @override
-  void onClose() {
-    super.onClose();
-  }
-
-  void increment() => count.value++;
-}
+class ProjectsDetailsController extends GetxController {}
